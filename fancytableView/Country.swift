3fe8587996f1227bae802498b44923b2415fcf45
Fayik99 <fayik99@gmail.com>
2020-07-13 16:45:30 +0530

    Countries added

---
+++ fancytableView/Country.swift
@@ -0,0 +1,12 @@
+//
+//  Country.swift
+//  fancytableView
+//
+//  Created by Fayik Muzammil on 7/13/20.
+//  Copyright © 2020 Fayik Muzammil. All rights reserved.
+//
+
+struct Country {
+    var code: String
+    var name: String
+}
